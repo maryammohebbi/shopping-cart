3636e89da369f5d7153283fb09c3be7e8a7a4398
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2022-08-22 22:21:13 +0430

    add to cart script added

---
+++ js/products.js
@@ -0,0 +1,57 @@
+export const productsData = [
+    {
+        id: 1,
+        title: "queen panel bed",
+        price: 12.59,
+        imageUrl: "../images/product-1.jpeg",
+    },
+
+    {
+        id: 2,
+        title: "new gesign product",
+        price: 16.47,
+        imageUrl: "../images/product-2.jpeg",
+    },
+
+    {
+        id: 3,
+        title: "white closet",
+        price: 10.47,
+        imageUrl: "../images/product-3.jpeg",
+    },
+
+    {
+        id: 4,
+        title: "new brown bed",
+        price: 13.21,
+        imageUrl: "../images/product-3.jpeg",
+    },
+
+    {
+        id: 5,
+        title: "chair and table",
+        price: 23.41,
+        imageUrl: "../images/product-5.jpeg",
+    },
+
+    {
+        id: 6,
+        title: "yellow new design",
+        price: 11.62,
+        imageUrl: "../images/product-6.jpeg",
+    },
+
+    {
+        id: 7,
+        title: "product new carpet",
+        price: 15.14,
+        imageUrl: "../images/product-7.jpeg",
+    },
+
+    {
+        id: 8,
+        title: "table and flower pot",
+        price: 19.52,
+        imageUrl: "../images/product-8.jpeg",
+    },
+];
--- js/script.js
+++ js/script.js
@@ -1,19 +1,94 @@
 const cartBtn = document.querySelector(".cart-icon");
 const backdrop = document.querySelector(".backdrop");
-const cart = document.querySelector(".cart");
+const myCart = document.querySelector(".cart");
+const productsDOM = document.querySelector(".products-center");
+
+const cart = [];
+
+import {productsData} from "./products.js";
+
+// 1. get products
+class Products {
+    getProduct (){
+        return productsData;
+    }
+}
+
+// 2. display products
+class UI {
+    displayProducts(products){
+        let result ="";
+        products.forEach(item => {
+            result += `<section class="product">
+                        <div class="img-container">
+                        <img class="product-img" src= ${item.imageUrl} alt="product 1">
+                        </div>
+                        <div class="products-desc">
+                            <p class="product-title">${item.title}</p>
+                            <p class="product-price">${item.price} $</p>
+                        </div>
+                        <button class="add-to-cart" data-id=${item.id}>
+                            <i class="fa-solid fa-cart-arrow-down"></i>
+                            add to cart
+                        </button>
+                    </section>`
+                    productsDOM.innerHTML = result;
+        });
+    }
+    getAddToCartBtns(){
+        const addToCartBtns = document.querySelectorAll(".add-to-cart");
+        // console.log(addToCartBtns);
+        const buttons = [...addToCartBtns];
+        // console.log(buttons);
+        buttons.forEach(btn => {
+            const id = btn.dataset.id;
+            // console.log(id)
+            const isInCart = cart.find(p => p.id === id);
+            if (isInCart){
+                btn.innerText = "In Cart";
+                btn.disable = "true";
+            }
+
+            btn.addEventListener("click", (event) => {
+                console.log(event.target.dataset.id);
+            })
+        })
+    }
+}
+
+// 3. storage
+class Storage {
+   static saveProducts(products){
+        localStorage.setItem("products", JSON.stringify(products))
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const products = new Products();
+    const productsData = products.getProduct();
+    // console.log(productsData);
+    const ui = new UI();
+    ui.displayProducts(productsData);
+    ui.getAddToCartBtns();
+    Storage.saveProducts(productsData);
+
+})
 
 cartBtn.addEventListener("click", showCartModal);
 backdrop.addEventListener("click", closeCartModal);
 
 function showCartModal(){
-    cart.style.top= "20%";
-    cart.style.opacity= "1";
+    myCart.style.top= "20%";
+    myCart.style.opacity= "1";
     backdrop.style.display= "block";
 
 }
 
 function closeCartModal(){
-    cart.style.top= "-100%";
-    cart.style.opacity= "0";
+    myCart.style.top= "-100%";
+    myCart.style.opacity= "0";
     backdrop.style.display= "none";
 }
